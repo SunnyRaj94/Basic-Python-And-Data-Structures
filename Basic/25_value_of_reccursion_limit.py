b42f77c0889841a18ab6e42dfe06d698ca9aa968
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:04:50 +0530

    program to get the current value of the recursion limit

---
+++ Basic/25_value_of_reccursion_limit.py
@@ -0,0 +1,13 @@
+"""
+Created on 12/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to get the current value of the recursion limit
+"""
+#importing sys module
+import sys
+#getting the limit of current reccursion
+print("Current value of the recursion limit:")
+print(sys.getrecursionlimit())
