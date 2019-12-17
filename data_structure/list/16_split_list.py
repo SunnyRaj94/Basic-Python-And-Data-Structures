2656c0755f3748c055c9a64c13266c27baab283b
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:22:33 +0530

    program to split a list based on first character of word

---
+++ data_structure/list/16_split_list.py
@@ -0,0 +1,14 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to split a list based on first character of word. 
+"""
+#initializing a list of strings or words
+list_of_words = ['Sunny','Ria','YuvrajSingh', 'BrandonMcculum','Lb','ABD','MSD','Ronaldo']
+#obtaining splitted list
+splitted_list=sorted(list_of_words)
+#printing the splitted list
+print(splitted_list)
