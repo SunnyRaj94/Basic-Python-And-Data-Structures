5289d0ec986b4d218c3837ce09f44d3b0b5f7b07
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:36:37 +0530

    program to create a tuple

---
+++ data_structure/tuple/01_create_tuple.py
@@ -0,0 +1,14 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to create a tuple
+"""
+#A tuple is a collection which is ordered and unchangeable.
+# In Python tuples are written with round brackets.
+#initializing a tuple
+thistuple = ("apple", "banana", "cherry")
+#printing the tuple output
+print(thistuple)
