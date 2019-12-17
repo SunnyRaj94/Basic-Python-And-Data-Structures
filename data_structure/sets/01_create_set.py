252d6a0c9f6bd050d6e9670ac1250057cf6f1dfe
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:23:57 +0530

    program to create a set

---
+++ data_structure/sets/01_create_set.py
@@ -0,0 +1,12 @@
+"""
+Created on 13/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to create a set
+"""
+#creating a set
+my_set=set({"apple", "banana", "cherry","potato","tomato"})
+#printing the output
+print(my_set)
