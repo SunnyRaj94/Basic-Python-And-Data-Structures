2470bf03c4d4ae5f009921b4360dfe60be907e18
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:24:27 +0530

    program to iteration over sets

---
+++ data_structure/sets/02_iteration_over_set.py
@@ -0,0 +1,13 @@
+"""
+Created on 13/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to iteration over sets.
+"""
+#creating a set
+my_set=set({"apple", "banana", "cherry","potato","tomato"})
+#iterating through set
+for item in my_set:
+    print(item)#printing the items inside set
