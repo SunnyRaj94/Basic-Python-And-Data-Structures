ecc1967863817b4a332a9747b3b82ea37ccd51a4
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:01:47 +0530

    program to get an absolute file path

---
+++ Basic/18_get_absolute_file_path.py
@@ -0,0 +1,12 @@
+"""
+Created on 11/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to get an absolute file path.
+"""
+#importing os inbuilt module
+import os
+#printing the absolute path of file
+print(os.path.abspath("05_calender.py"))
