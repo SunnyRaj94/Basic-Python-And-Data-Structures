2bca610277c89764172f3ae9be5ed4626083d9c1
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 12:54:10 +0530

    program to print the documents (syntax, description etc.) of Python built-in function(s)

---
+++ Basic/04_syntax.py
@@ -0,0 +1,13 @@
+"""
+Created on 11/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement :
+Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s). 
+"""
+# taking input
+default_function = input("Enter default functions")
+
+# using help function
+help(default_function)
