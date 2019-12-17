83ffdf262218b82b02a0aac6ce5abe326d8e7df6
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:13:39 +0530

    program to determine if the python shell is executing in 32bit or 64bit mode on operating system

---
+++ Basic/42_python_shell.py
@@ -0,0 +1,13 @@
+"""
+Created on 12/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to determine if the python shell is executing in 32bit or 64bit mode on operating system
+"""
+#importing platform module
+import platform
+
+#printing platform detail
+print(platform.architecture())
