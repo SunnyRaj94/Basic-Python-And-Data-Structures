b4dd5c03f2e98576d4096d997ced7227f2b358d2
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:02:38 +0530

    program to sort three integers without using conditional statements and loops

---
+++ Basic/20_sort_integers.py
@@ -0,0 +1,12 @@
+"""
+Created on 11/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+ Write a Python program to sort three integers without using conditional statements and loops. 
+"""
+#initializing variable
+integers=[3,2,1]
+#printing the sorted set
+print(sorted(integers))
