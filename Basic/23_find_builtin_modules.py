a5b92393f4db0efd3da2ef2fa403d8ae619bad2c
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:03:57 +0530

    program to find the available built-in modules

---
+++ Basic/23_find_builtin_modules.py
@@ -0,0 +1,10 @@
+"""
+Created on 12/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to find the available built-in modules.
+"""
+# printing the list of available  built in modules
+print(help('modules'))
