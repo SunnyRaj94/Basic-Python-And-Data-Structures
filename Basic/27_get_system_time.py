e0e312db659c5368c34d7dfd1e2aacf99c541765
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:05:42 +0530

    program to get the system time

---
+++ Basic/27_get_system_time.py
@@ -0,0 +1,14 @@
+"""
+Created on 12/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to get the system time
+"""
+# importing date time module
+import datetime
+#getting system time
+system_time = datetime.datetime.now()
+#printing the value
+print(system_time)
