a777dd4e63f8a782d6f7b47d4bef92b33fb1ef80
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:27:11 +0530

    program to create an intersection of sets

---
+++ data_structure/sets/06_intersection_of_set.py
@@ -0,0 +1,16 @@
+"""
+Created on 13/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+ Write a Python program to create an intersection of sets. 
+"""
+#creating two sample sets
+my_first_set={"apple", "banana", "cherry","potato","xyz"}
+my_second_set={"apple","banana"}
+#returning intersection of above sets
+intersection_set = my_first_set.intersection(my_second_set)
+
+#printing the intersection of sets
+print(intersection_set)
