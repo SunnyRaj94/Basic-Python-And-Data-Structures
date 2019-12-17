9d5eac5df6b246a3dd00f052c2a0373e12da63d6
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:08:53 +0530

    program to get the users environment

---
+++ Basic/33_get_user_env.py
@@ -0,0 +1,12 @@
+"""
+Created on 12/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to get the users environment.
+"""
+#importing os inbuilt module
+import os
+#printing user's environment
+print(os.environ)
