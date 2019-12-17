db936dc53c134d2da2edf8be93cb4b957a28fbd3
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:29:06 +0530

    Write a Python program to clear a set

---
+++ data_structure/sets/10_clear_set.py
@@ -0,0 +1,14 @@
+"""
+Created on 13/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to clear a set. 
+"""
+#creating sample set
+my_set={"apple", "banana", "cherry","potato","xyz"}
+#clearing the set
+my_set.clear()
+#printing the cleared set
+print(my_set)
