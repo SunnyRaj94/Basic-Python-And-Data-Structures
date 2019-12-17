d46183256d7abec45cbf22cbd6094e0f3668b85a
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 13:00:46 +0530

    Write a Python program to get the current username

---
+++ Basic/16_get_currentuser_username.py
@@ -0,0 +1,13 @@
+"""
+Created on 11/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to get the current username 
+"""
+#importing os inbuilt module
+import os
+
+#getting current user username
+print(os.uname())
