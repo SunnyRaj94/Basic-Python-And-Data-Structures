b9804b20fa6f9abef77a308ff80f28dd63ca26c6
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:18:30 +0530

    program to clone or copy a list

---
+++ data_structure/list/07_clone_list.py
@@ -0,0 +1,14 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to clone or copy a list
+"""
+#creating a list through user input
+integer_list = [int(integer) for integer in input("enter integers").split(',')]
+#making a copy if interger list
+copy_of_integer_list=integer_list.copy()
+#printing it to the consoles
+print(copy_of_integer_list)
