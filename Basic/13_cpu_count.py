ebe5be1cc3397c04f1fae9ef2d7bdfb4889d3e7e
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 12:58:32 +0530

    program to find out the number of CPUs using

---
+++ Basic/13_cpu_count.py
@@ -0,0 +1,13 @@
+"""
+Created on 11/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a Python program to find out the number of CPUs using. 
+"""
+#importing os inbuilt module
+import os
+
+# counting no of cpus
+print(os.cpu_count())
