3e3c2646ab200e40dc775e5d7c59cae39bfd8ba4
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:29:54 +0530

    program to find maximum and the minimum value in a set

---
+++ data_structure/sets/12_max_and_min_in_set.py
@@ -0,0 +1,21 @@
+"""
+Created on 13/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to find maximum and the minimum value in a set
+"""
+#creating sample set
+my_set=set({"apple", "banana", "cherry","potato","xyz"})
+
+#finding maximum value from set
+maximum_value = max(my_set)
+
+# finding minimum value from set
+minimum_value= min(my_set)
+
+#printing maximum value obtained
+print(maximum_value)
+#printing minimum value obtained
+print(minimum_value)
