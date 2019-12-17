4bd28c85302da92845d1146b49d411abb70d15ab
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:39:10 +0530

    program to convert a list to a tuple

---
+++ data_structure/tuple/07_list_to_tuple.py
@@ -0,0 +1,14 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to convert a list to a tuple. 
+"""
+#initializing a list of strings or words
+list_of_words = ['Sunny','Ria','YuvrajSingh', 'BrandonMcculum','Lb','ABD','MSD','Ronaldo']
+#converting this list into tuple
+tuple_of_list=tuple(list_of_words)
+#printing the result
+print(tuple_of_list)
