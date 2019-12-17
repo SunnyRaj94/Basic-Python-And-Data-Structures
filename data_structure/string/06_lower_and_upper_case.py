c591340cfe10cdc2890cc52c69a2720cdeccfe9e
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:32:55 +0530

    script that takes input from the user and displays that input back in upper and lower cases

---
+++ data_structure/string/06_lower_and_upper_case.py
@@ -0,0 +1,17 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python script that takes input from the user and displays that input back in upper and lower cases
+"""
+#asking from user to enter a string
+my_string = input("enter any string")
+
+#printing string in lower case
+print("entered string in lower case")
+print(my_string.lower())
+#printing string in uppercase
+print("entered string in upper case")
+print(my_string.upper())
