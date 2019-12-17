80a31591473965cff55d39eb3a332c8f3e92baad
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 12:58:00 +0530

    program to call an external command in Python

---
+++ Basic/12_external_command.py
@@ -0,0 +1,13 @@
+"""
+Created on 11/12/2019
+@author: Sunny Raj
+"""
+"""
+Problem Statement:
+Write a python program to call an external command in Python. 
+"""
+#importing os inbuilt module
+import os
+
+#calling for an external command
+os.system("ls")
