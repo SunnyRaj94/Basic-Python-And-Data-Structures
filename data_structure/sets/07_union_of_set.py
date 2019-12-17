b4a82a5ecaa4c9f339e7c75afafdfc7d86b9eacb
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:27:56 +0530

    program to create an union of sets

---
+++ data_structure/sets/07_union_of_set.py
@@ -0,0 +1,16 @@
+"""
+Created on 13/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+ Write a Python program to create an union of sets. 
+"""
+#creating two sample sets
+my_first_set={"apple", "banana", "cherry","potato","xyz"}
+my_second_set={"apple","banana"}
+#returning union of above sets
+union_set = my_first_set.union(my_second_set)
+
+#printing the intersection of sets
+print(union_set)
