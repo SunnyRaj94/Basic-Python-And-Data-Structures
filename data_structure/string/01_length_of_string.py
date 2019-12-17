28792b46497b370b3e48b9e4f7ef3124896b77b5
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:30:28 +0530

    calculate the length of a string

---
+++ data_structure/string/01_length_of_string.py
@@ -0,0 +1,12 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+ Write a Python program to calculate the length of a string
+"""
+#asking from user to enter a string
+my_string = input("enter any string")
+#calculating and displaying length of entered value
+print("length of your entered string is : ",len(my_string))
