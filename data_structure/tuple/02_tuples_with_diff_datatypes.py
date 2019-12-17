b07dc6c75afea8c4f198859401703bc8ec117a62
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:37:10 +0530

    program to create a tuple with different data types

---
+++ data_structure/tuple/02_tuples_with_diff_datatypes.py
@@ -0,0 +1,12 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to create a tuple with different data types.
+"""
+#initializing a tuple with different data types
+my_tuple =(10,'sunny',22.3,True,1+3j,'h')
+#printing tuple with different datatypes
+print(my_tuple)
