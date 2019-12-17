a2beb10e9a06d077ccbb314a9424432fc9dd182f
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 14:39:56 +0530

    program to slice a tuple

---
+++ data_structure/tuple/09_slice_tuple.py
@@ -0,0 +1,16 @@
+"""
+Created on 14/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a Python program to slice a tuple.
+"""
+#initializing a tuple with different data types
+my_tuple =(10,'sunny',22.3,True,1+3j,'h')
+
+#Slicing the values from a tuple
+#showing output
+print(my_tuple[0:4])
+
+print(my_tuple[::2])
