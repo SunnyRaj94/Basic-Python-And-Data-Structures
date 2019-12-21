9ac465c33cbf39f04d4048593ea53fd9ac34cabf
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:04:05 +0530

    refactored:sum all the items in a list

--- data_structure/list/01_sum_of_items.py
+++ data_structure/list/01_sum_of_items.py
@@ -9,7 +9,9 @@ Write a Python program to sum all the items in a list.
 #creating a list through user input
 integer_list = [int(integer) for integer in input("enter integers").split(',')]
 #storing the value obtained from sum fumction
-sum_of_integers =sum(integer_list)
+sum_of_integers =0
+for value in integer_list:
+    sum_of_integers=sum_of_integers+value
 
 #printing the sum obtained
 print("sum of integers in input list is : ",sum_of_integers)
