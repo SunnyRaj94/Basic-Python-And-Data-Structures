3b6bc2b97aa0bedc7933bdeb97afcdc0136a412d
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:03:05 +0530

    refactored:check multiple keys exists in a dictionary

--- data_structure/dictionary/12_check_multiple_values.py
+++ data_structure/dictionary/12_check_multiple_values.py
@@ -7,16 +7,16 @@ problem statement:
 Write a Python program to check multiple keys exists in a dictionary.
 """
 #initializing a dictionary
-my_dictionary={"key1":"value1","key5":"value5","key4":"value4","key3":"value3","key2":"value2"}
+my_dictionary={'val_1':8,'val_2':21,'val_3':7,'val_4':20,'val_5':47,'val_6':53,'val_7':29,'val_8':88,'val_9':38}
 try:
     #asking user to enter the key name he wants to check for
-    check_key=input("enter the key name you want to search for")
+    check_key=list({'val_1', 'val_2', 'val_3', 'val_4', 'val_5', 'val_6', 'val_7', 'val_8','val'})
     #obtaining result from get method on dictionary
-    result =my_dictionary.get(check_key)
+    result =  all(elem in list(my_dictionary.keys()) for elem in check_key)
     #checking if result is none
-    if result == None:
-        print("key is not present in dictionary")
+    if result :
+        print("'Given keys are present in the Dictionary'")
     else:
-        print("key is present in the dictionary")
+        print("'Given keys are not present in the Dictionary'")
 except Exception:
     print("some error occured")
