cb1eff2055daee9b43ba612f7481052123f156ac
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:24:50 +0530

    print a specified list after removing the 0th, 4th and 5th elements

--- data_structure/list/10_remove_indexes.py
+++ data_structure/list/10_remove_indexes.py
@@ -10,8 +10,11 @@ Write a Python program to print a specified list after removing the 0th, 4th and
 list_of_words = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 
 #removing elements from given indexes
-list_of_words.pop(0)
-list_of_words.pop(3)
-list_of_words.pop(3)
+new_list_of_words =[]
+for index in range(len(list_of_words)):
+    if index==0 or index==4 or index==5:
+        pass
+    else:
+        new_list_of_words.append(list_of_words[index])
 #printing the output
-print(list_of_words)
+print(new_list_of_words)
