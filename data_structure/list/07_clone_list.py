5b3b19a449df9f7da779733809791d289918d0a4
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:24:17 +0530

    refactored:clone or copy a list

--- data_structure/list/07_clone_list.py
+++ data_structure/list/07_clone_list.py
@@ -9,6 +9,8 @@ Write a Python program to clone or copy a list
 #creating a list through user input
 integer_list = [int(integer) for integer in input("enter integers").split(',')]
 #making a copy if interger list
-copy_of_integer_list=integer_list.copy()
+copy_of_integer_list=[]
+for value in integer_list:
+    copy_of_integer_list.append(value)
 #printing it to the consoles
 print(copy_of_integer_list)
