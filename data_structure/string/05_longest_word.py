c923b86d688bc5de14a0406d91bb612976bd3f58
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:50:34 +0530

    refactored: returned longest word from string

--- data_structure/string/05_longest_word.py
+++ data_structure/string/05_longest_word.py
@@ -9,7 +9,10 @@ Write a Python function that takes a list of words and returns the length of the
 #asking from user to enter a string
 my_string = input("enter any string")
 #making a list by splitting it through spaces
-my_string_list = my_string.split()
-
+my_string_list = my_string.split(' ')
+longest_word =my_string_list[0]
+for index in range(len(my_string_list)):
+    if len(longest_word)<len(my_string_list[index]):
+        longest_word=my_string_list[index]
 #printing word of maximum length
-print(max(my_string_list))
+print(longest_word)
