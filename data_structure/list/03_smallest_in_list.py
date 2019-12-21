491bfcceff32fe469857e3d6e1afe2ba6a0797b0
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:04:48 +0530

    refactored:get the smallest number from a list

--- data_structure/list/03_smallest_in_list.py
+++ data_structure/list/03_smallest_in_list.py
@@ -10,7 +10,11 @@ Write a Python program to get the smallest number from a list
 integer_list = [int(integer) for integer in input("enter integers").split(',')]
 
 #storing the smallest number in the list into a varible
-smallest_value = min(integer_list)
+smallest_value = integer_list[0]
+for value in integer_list:
+    if value>smallest_value:
+        smallest_value=value
+
 
 #printing the smallest value
 print(smallest_value)
