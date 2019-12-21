da440f934582c2418ff9d5564fc387e61eb89e90
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:48:57 +0530

    refactored:count the number of characters (character frequency) in a string

--- data_structure/string/02_count_char_frequency.py
+++ data_structure/string/02_count_char_frequency.py
@@ -4,16 +4,26 @@ Created on 14/12/2019
 """
 """
 problem statement:
-Write a Python program to count the number of characters (character frequency) in a 
+Write a Python program to count the number of characters (character frequency) in a string
 """
-#asking from user to enter a string
-my_string = input("enter any string")
-#initializing a dictionary  to store character frequecy
-frequency_dict={}
-#looping through the index of string
-for characters in my_string:
-    #storing it to dictionary
-    frequency_dict[characters]=my_string.count(characters)
+def my_function():
+    # asking from user to enter a string
+    my_string = input("enter any string")
+    # initializing a dictionary  to store character frequecy
+    frequency_dict = {}
+    # looping through the index of string
+    for characters in my_string:
+        # storing it to dictionary
+        frequency_dict[characters] = count_char(my_string,characters)
 
-#printing the dictionary output
-print(frequency_dict)
+    # printing the dictionary output
+    print(frequency_dict)
+
+def count_char(my_string,char):
+    count =0
+    for val in my_string:
+        if val == char:
+            count=count +1
+    return count
+
+my_function()
