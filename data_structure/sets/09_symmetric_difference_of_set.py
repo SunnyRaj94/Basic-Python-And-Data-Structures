4659db116a7ccb21fc9344a1b6839d14a2af2e9f
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-21 12:34:27 +0530

    refactored:create a symmetric difference

--- data_structure/sets/09_symmetric_difference_of_set.py
+++ data_structure/sets/09_symmetric_difference_of_set.py
@@ -10,6 +10,6 @@ Write a Python program to create a symmetric difference.
 my_first_set={"apple", "banana", "cherry","potato","xyz"}
 my_second_set={"apple","banana"}
 #returning symmetric difference of above sets
-symmetric_difference_set = my_first_set.symmetric_difference(my_second_set);
+symmetric_difference_set = my_first_set ^ my_second_set
 #printing the symmetric difference set
 print(symmetric_difference_set)
